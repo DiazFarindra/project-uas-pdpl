2e3c32cfa2b59c2ee92100d67d961e409abe7bfc
diaz farindra <farindrad@gmail.com>
2025-01-09 13:35:18 +0700

    update

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -1,16 +1,11 @@
 <nav class="navbar navbar-expand-lg bg-body-secondary">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('admin.dashboard') }}">Admin V-Payment</a>
+        <a class="navbar-brand" href="{{ route('admin.items.index') }}">Admin V-Payment</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link {{ route('admin.dashboard') === url()->current() ? 'active' : '' }}" href="{{ route('admin.dashboard') }}">
-                        dashboard
-                    </a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link {{ route('admin.items.index') === url()->current() ? 'active' : '' }}" href="{{ route('admin.items.index') }}">
                         items
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-secondary">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('admin.dashboard') }}">V-Payment</a>
+        <a class="navbar-brand" href="{{ route('admin.items.index') }}">V-Payment</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
